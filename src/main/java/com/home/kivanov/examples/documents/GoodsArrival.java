c831dcbba2f81a3526bc9bbd40eee8d82f43aaac
Konstantyn <s.ivanov.konstantyn@gmail.com>
2019-11-02 07:21:42 +0200

    refactoring

--- src/main/java/com/home/kivanov/examples/documents/GoodsArrival.java
+++ src/main/java/com/home/kivanov/examples/documents/GoodsArrival.java
@@ -2,7 +2,6 @@ package com.home.kivanov.examples.documents;
 
 import com.home.kivanov.examples.goods.StorageItem;
 import com.home.kivanov.examples.services.StorageService;
-import com.home.kivanov.examples.services.StorageServiceImpl;
 
 import java.time.LocalDateTime;
 import java.util.List;
