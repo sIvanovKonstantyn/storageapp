75ee0bc00795320d35e7fb7772d99cfc0164dbe8
Konstantyn <s.ivanov.konstantyn@gmail.com>
2019-11-02 07:10:30 +0200

    Change business logic of inventory

--- src/main/java/com/home/kivanov/examples/documents/Inventory.java
+++ src/main/java/com/home/kivanov/examples/documents/Inventory.java
@@ -1,38 +1,17 @@
 package com.home.kivanov.examples.documents;
 
 import com.home.kivanov.examples.goods.StorageItem;
-import com.home.kivanov.examples.services.StorageServiceImpl;
+import com.home.kivanov.examples.services.StorageService;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Inventory extends AbstractStorageDocument {
-    public Inventory(StorageServiceImpl storage, String number, LocalDateTime dateTime, List<StorageItem> storageItems) {
+    public Inventory(StorageService storage, String number, LocalDateTime dateTime, List<StorageItem> storageItems) {
         super(storage, number, dateTime, storageItems);
     }
 
     public List<StorageItem> calculate() {
-
-        final List<StorageItem> storageItems = storage.findAll();
-
-        return goods
-                .stream()
-                .map(storageItem -> {
-
-                    final StorageItem item = storageItems
-                            .stream()
-                            .filter(currentItem -> currentItem.equals(storageItem))
-                            .findAny()
-                            .orElse(null);
-
-                    if (item == null) {
-                        return new StorageItem(storageItem.getId(), storageItem.getGoods(), -storageItem.getCount());
-                    } else {
-                        return new StorageItem(storageItem.getId(), storageItem.getGoods(), item.getCount() - storageItem.getCount());
-                    }
-
-                })
-                .collect(Collectors.toList());
+        return storage.findAll();
     }
 }
